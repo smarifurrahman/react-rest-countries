2738e88364e306d377e32188916af6394114838a
S M Arifur Rahman <smarifurrahman82@gmail.com>
2023-03-25 01:03:20 +0600

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import Countries from './components/Countries/Countries';
+import Header from './components/Header/Header';
+import Person from './components/Person/Person';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Countries></Countries>
+      <Person></Person>
     </div>
   );
 }
+
+/* function LoadCountries() {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all')
+      .then(res => res.json())
+      .then(data => setCountries(data))
+  }, [])
+
+  return (
+    <div>
+      <h1>Visiting Every Country of the World</h1>
+      <h3>Available Countries: {countries.length}</h3>
+      {
+        countries.map(country => <p>{country.name.common}</p>)
+      }
+    </div>
+  );
+} */
 
 export default App;
